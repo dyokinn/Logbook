02413de9d23962410222dfbf28cdaf9c22385428
Di-santos <dio.silveira07@gmail.com>
2021-10-02 23:03:24 -0300

    Starting state management in the login logic

--- lib/modules/screens/home_page.dart
+++ lib/modules/screens/home_page.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:logbook/shared/controllers/login_controller.dart';
+import 'package:logbook/shared/providers/user_provider.dart';
 import 'package:logbook/shared/theme/main_colors.dart';
+import 'package:logbook/shared/theme/text_styles.dart';
+import 'package:provider/src/provider.dart';
 
 class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
 
-    final LoginController loginController = LoginController();
+    final state = context.watch<userState>();
 
     return Scaffold(
-      backgroundColor: MainColors.black,
-      body: Center(
-        child: Text("Logou")
-      ),
+      backgroundColor: MainColors.blue,
+      body: Container(
+        height: 700,
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(state.userInfo["photo"]),
+            Text("Logou", style: TextStyles.title),
+            Text(state.userInfo["email"], style: TextStyles.text),
+            Text(state.userStats["mental"].toString(), style: TextStyles.text),
+            ElevatedButton(
+              child: Text("aoo"),
+              onPressed: () => state.setUserInfoField("email", "Fábio Almeida"),
+            )
+          ],
+        ),
+      )
     );
   }
 }
--- lib/shared/controllers/login_controller.dart
+++ lib/shared/controllers/login_controller.dart
@@ -1,19 +1,35 @@
+import 'dart:convert';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:injector/injector.dart';
+import 'package:logbook/shared/providers/user_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase/supabase.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:provider/provider.dart';
+
 
 class LoginController{
 
   late final supabase = Injector.appInstance.get<SupabaseClient>();
 
+  // Checa se o usuário já logou anteriormente
+  Future<void> userAlreadyLogged(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
 
-  void userAlreadyLogged(BuildContext context) async {
-    //Navigator.pushReplacementNamed(context, "/login");
+    if (prefs.containsKey("userInfo") && prefs.containsKey("userStats")){
+      final userProvider = context.read<userState>();
+      userProvider.setUserInfo(jsonDecode(await prefs.getString("userInfo") as String));
+      userProvider.setUserStats(jsonDecode(await prefs.getString("userStats") as String));
+      Navigator.pushReplacementNamed(context, "/home");
+    }
+
+    else{
+      Navigator.pushReplacementNamed(context, "/login");
+    }
   }
 
-  Future<void> handleLogin() async {
+  // Gerencia o login e salva as informações do usuário nas SP
+  Future<void> handleLogin(BuildContext context) async {
     GoogleSignIn _googleSignIn = GoogleSignIn(
         scopes: [
           'email'
@@ -21,16 +37,95 @@ class LoginController{
       );
     
     try {
-      final user = await _googleSignIn.signIn();
+      // Tenta logar com o Google
+      final googleUser = await _googleSignIn.signIn();
 
-      final response = await supabase
+      // Verifica se o usuário autenticado já existe no banco
+      final accountFromDb = await supabase
                         .from("users")
                         .select()
+                        .eq("email", googleUser?.email.toString())
                         .execute();
-    
-      print("ABAIXO");
-      print(response.data);
-    
+      print(googleUser?.email.toString());
+      print(accountFromDb.data);
+
+      // Se não existir no banco
+      if (accountFromDb.data.length == 0){
+
+        try{
+          final created = await supabase
+                            .from("users")
+                            .insert({
+                              "email": googleUser?.email,
+                              "name": googleUser?.displayName
+                            })
+                            .execute();
+          // Salva no Provider
+          final prefs = await SharedPreferences.getInstance();
+          final userProvider = context.read<userState>();
+
+          Map<String, dynamic> userInfo = {
+            "id": created.data[0]["id"],
+            "name": created.data[0]["name"],
+            "email": created.data[0]["email"],
+            "photo": googleUser?.photoUrl
+          };
+
+          Map<String, double> userStats = {
+            "mental": 5/10,
+            "physical": 5/10,
+            "professional": 5/10,
+            "social": 5/10
+          };
+
+          userProvider.setUserInfo(userInfo);
+          userProvider.setUserStats(userStats);
+
+          // Salva nas Shared Preferences
+          await prefs.setString("userInfo", jsonEncode(userInfo));
+          await prefs.setString("userStats", jsonEncode(userStats));
+
+          Navigator.pushReplacementNamed(context, "/home");
+
+        }catch(error){
+          print("deu pau e não existe");
+        }
+      }
+      // Se já existir no banco
+      else{
+        try{
+          // Salva no Provider
+          final prefs = await SharedPreferences.getInstance();
+          final userProvider = context.read<userState>();
+
+          Map<String, dynamic> userInfo = {
+            "id": accountFromDb.data[0]["id"],
+            "name": accountFromDb.data[0]["name"],
+            "email": accountFromDb.data[0]["email"],
+            "photo": googleUser?.photoUrl
+          };
+
+          Map<String, double> userStats = {
+            "mental": 5/10,
+            "physical": 5/10,
+            "professional": 5/10,
+            "social": 5/10
+          };
+
+          userProvider.setUserInfo(userInfo);
+          userProvider.setUserStats(userStats);
+
+          // Salva nas Shared Preferences
+          await prefs.setString("userInfo", jsonEncode(userInfo));
+          await prefs.setString("userStats", jsonEncode(userStats));
+
+          Navigator.pushReplacementNamed(context, "/home");
+
+        }catch(error){
+          print("deu pau e já existe");
+          print(error);
+        }
+      }
     } 
     
     catch (error) {
