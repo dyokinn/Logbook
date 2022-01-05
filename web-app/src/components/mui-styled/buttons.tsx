5805ea3548c133dfb0e2070239bcef506d1e9143
Di-santos <dio.silveira07@gmail.com>
2022-01-05 17:11:03 -0300

    saving some changes

--- web-app/src/components/mui-styled/buttons.tsx
+++ web-app/src/components/mui-styled/buttons.tsx
@@ -1,8 +1,13 @@
 import { Button, IconButton } from '@mui/material';
 import styled from 'styled-components';
 
-const StyledButton = styled(Button)<{backgroundColor: string}>`
+const StyledButton = styled(Button)<{backgroundColor: string, textColor: string}>`
     background-color: ${props => props.backgroundColor};
+    color: ${props => props.textColor};
+    &:hover{
+        background-color: red;
+    }
+
 `
 
 const StyledIconButton = styled(IconButton)`
--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -3,15 +3,38 @@ import ship from '../../public/images/navio.jpg'
 import Image from 'next/image'
 import NavBar from '../components/home/navBar'
 import Card from '../components/home/card'
-import { FiAnchor } from "react-icons/fi";
+import { FiAnchor, FiMap } from "react-icons/fi";
 import { useTheme } from '../hooks/useTheme'
+import styles from "../styles/index.module.scss"
+import styled from 'styled-components'
+import { StyledButton } from '../components/mui-styled/buttons'
+
+const Background = styled.div<{color: string}>`
+    background-color: ${props => props.color};
+    margin: 0 !important;
+    padding: 0 !important;
+    font-family: 'Poppins', sans-serif;
+`
 
 const Home: NextPage = () => {
   const {mode, theme, setModeOpt} = useTheme()
+  
   return (
-    <>
+    <Background color={theme.second}>
         <NavBar/>
-    </>
+        <div id={styles.welcome}>
+            <div>
+                <h1 style={{color: `{theme}`}}>Permita-nos acompanhar sua viagem!</h1>
+                <p>Guarde suas ações, emoções, relatos e histórico em uma só plataforma</p>
+                <StyledButton backgroundColor={theme.blue} textColor={theme.text} endIcon={<FiMap/>}>
+                    Comece a navegar!
+                </StyledButton>
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </Background>
   )
 }
 
